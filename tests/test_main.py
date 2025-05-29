66910e10e35ff9a20747f6034f8b0d5a7c991ed2
gustavokubiack <guukubiack@gmail.com>
2025-05-29 09:10:28 -0300

    test: ajusta teste conforme nova mensagem da rota padrão

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello World :)"}
 
 def test_square():
     response = client.get("/square/3")
